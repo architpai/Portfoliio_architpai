a3c812ca98433b081333be0ea47dc6f4cdadf132
Archit Pai <pai.archit5@gmail.com>
2023-01-13 14:44:38 +0900

    init

---
+++ src/data.js
@@ -0,0 +1,36 @@
+export const projects = [
+  {
+    title: "Reporting Dashboard",
+    subtitle: "Python React",
+    description:
+      "This App uses Dash By plotly to create an analytical web application which analyzes the classified incident records and generates interactive data visualizations which help gather meaningful insights from the data. The DashBoard produces interactive visualizations which enable the user to drill down to the root cause of the incident and it does so by using custom cross filters put in place considering the desired output of the individual.",
+    image: "./project-1.gif",
+    link: "https://architpai.pythonanywhere.com/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "SQL",
+  "Node",
+  "Python",
+  "Material UI",
+];
